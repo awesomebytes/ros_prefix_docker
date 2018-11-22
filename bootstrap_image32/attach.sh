1a186bac0a36ec94fe728eff881a541b103a6646
Sammy Pfeiffer <Sammy.Pfeiffer@student.uts.edu.au>
2018-11-22 23:53:34 +1100

    Experimental 32bits support

---
+++ bootstrap_image32/attach.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Guessing running container name for ros_prefix_bootstrapped_image32..."
+running_name=$(docker ps | grep ros_prefix_bootstrapped_image32 | awk '{print $NF}')
+echo "Found running name: $running_name"
+echo "Attaching..."
+docker exec -it $running_name /bin/bash
---
+++ bootstrap_image32/push.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -ve
+# Need to have Docker logged in (docker login)
+
+image_id=$(docker images | grep ros_prefix_bootstrapped_image32 | awk '{print $1}')
+
+docker tag $image_id awesomebytes/ros_prefix_bootstrapped_image32:latest
+
+docker push awesomebytes/ros_prefix_bootstrapped_image32
